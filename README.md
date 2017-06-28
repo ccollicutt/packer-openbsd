7db39c87d57ba422704261b3b2445e3b701916ca
Curtis Collicutt <curtis@serverascode.com>
2017-06-27 21:54:55 -0600

    Working use of hostname and first ssh key

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 This repository contains templates for building OpenBSD templates for OpenStack using Packer.
 
+## Issues
+
+* No proper cloud-init support, only pulling the first ssh key from metadata
+* No resizing of disk, though you could specify the size of the disk in the packer configuration
+
 ## History and License
+
+This repository was forked from [tmatilai/packer-openbsd](https://github.com/tmatilai/packer-openbsd) but has been edited a fair amount and several files removed.
 
 The templates are based on the [Bento](https://github.com/opscode/bento)
 project and [Veewee](https://github.com/jedi4ever/veewee) templates.
--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -9,6 +9,7 @@ export PKG_PATH="$MIRROR/pub/OpenBSD/$uname_r/packages/`arch -s`/"
 echo "INFO: Adding PKG_PATH to /root/.profile"
 echo "export PKG_PATH=\"$PKG_PATH\"" >> /root/.profile
 
+# NOTE(curtis): Could use ftp command instead of installing curl if desired
 echo "INFO: Adding curl"
 pkg_add curl-7.53.1
 
@@ -19,3 +20,59 @@ echo "dhcp" >> /etc/hostname.vio0
 #echo "INFO: Adding cloud-init.pl"
 #ftp -o /usr/local/libdata/cloud-init.pl https://raw.githubusercontent.com/exoscale/openbsd-cloud-init/master/cloud-init.pl
 #perl /usr/local/libdata/cloud-init.pl deploy
+
+#
+# Cleanup
+#
+
+echo "INFO: Removing various files..."
+rm /etc/ssh/ssh_host*
+rm /etc/random.seed
+rm /var/db/host.random
+rm /etc/isakmpd/private/local.key
+rm /etc/isakmpd/local.pub
+rm /etc/iked/private/local.key
+#rm /etc/isakmpd/local.pub
+rm -rf /tmp/*
+rm /var/db/dhclient.leases.vio0
+
+echo "INFO: Disabling root password"
+chpass -a 'root:*:0:0:daemon:0:0:Charlie &:/root:/bin/ksh'
+
+echo "INFO: Adding /etc/firsttime with basic metadata gathering"
+
+# If you quote EOF then it won't evaluate backticks, which end up being empty
+# after an echo.
+cat << "EOF" > /etc/rc.firsttime
+#!/bin/sh
+META="http://169.254.169.254/latest/meta-data"
+C="/usr/local/bin/curl -s"
+
+i="1"
+# Test if we can hit the metadata URI
+while [ $i -lt 20 ]; do
+  echo "INFO: Attempt $i to curl metadata URI"
+  $C $META > /dev/null && break
+  i=`expr $i + 1`
+  sleep 2
+done
+echo "INFO: Was able to curl metadata"
+
+echo "INFO: Curling local-hostname"
+MYNAME=`$C $META/local-hostname | cut -f 1 -d "."`
+if [ -z "$MYNAME " ]; then
+  echo "ERROR: MYNAME is empty"
+else
+  echo "INFO: Adding hostname to /etc/myname"
+  echo $MYNAME > /etc/myname
+fi
+echo "INFO: Curling public-key"
+SSH_KEY=`$C $META/public-keys/0/openssh-key`
+if [ -z "$SSH_KEY" ]; then
+  echo "ERROR: SSH_KEY is empty"
+else
+  echo "INFO: adding ssh key to /root/.ssh/authorized_keys"
+  echo "$SSH_KEY" > /root/.ssh/authorized_keys
+  chmod 600 /root/.ssh/authorized_keys
+fi
+EOF
